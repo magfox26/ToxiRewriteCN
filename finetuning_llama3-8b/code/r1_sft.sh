527994b6156d5dfbd2df81aebe40dc5580384214
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-30 20:50:55 +0800

    Update r1_sft.sh

--- finetuning_llama3-8b/code/r1_sft.sh
+++ finetuning_llama3-8b/code/r1_sft.sh
@@ -1,10 +1,10 @@
 NPROC_PER_NODE=4 \
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 swift sft \
---model /models/Meta-Llama-3.1-8B-Instruct \
+--model /home/ToxiRewriteCN/models/Meta-Llama-3.1-8B-Instruct \ 
 --model_type llama3_1 \
 --train_type full \
---dataset /finetuning_llama3-8b/data/r1_train.json \
+--dataset /home/ToxiRewriteCN/finetuning_llama3-8b/data/r1_train.json \
 --num_train_epochs 5 \
 --per_device_train_batch_size 2 \
 --per_device_eval_batch_size 2 \
@@ -19,6 +19,6 @@ swift sft \
 --weight_decay 1e-4 \
 --deepspeed zero3 \
 --dataloader_num_workers 4 \
---output_dir /finetuning_llama3-8b/output/llama3_8b_r1 \
+--output_dir /home/ToxiRewriteCN/finetuning_llama3-8b/output/llama3_8b_r1 \
 --report_to swanlab \
---swanlab_token your_own_swanlab_token
+--swanlab_token your_own_swanlab_token 

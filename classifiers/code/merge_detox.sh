482de0537aa03aa7a1043839b51939463e3854de
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-30 21:31:00 +0800

    Create merge_detox.sh

---
+++ classifiers/code/merge_detox.sh
@@ -0,0 +1,4 @@
+swift export \
+    --adapters /home/ToxiRewriteCN/classifiers/output/detoxification_accuracy_full_data/ \ 
+    --merge_lora true \  
+    --output_dir /home/ToxiRewriteCN/classifiers/output/merged_detoxification_accuracy_full_data/ 

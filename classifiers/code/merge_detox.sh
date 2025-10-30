b0038bbb078a345d7f7ba1d6f39c72bb0199d959
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-30 22:16:18 +0800

    Update merge_detox.sh

--- classifiers/code/merge_detox.sh
+++ classifiers/code/merge_detox.sh
@@ -1,4 +1,4 @@
 swift export \
     --adapters /home/ToxiRewriteCN/classifiers/output/detoxification_accuracy_full_data/ \ 
     --merge_lora true \  
-    --output_dir /home/ToxiRewriteCN/classifiers/output/merged_detoxification_accuracy_full_data/ 
+    --output_dir /home/ToxiRewriteCN/classifiers/output/qwen3-32b-tox-classifier/ 

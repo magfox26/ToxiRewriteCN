d27004737bc84da126532e84e3ee5d29415f2bf2
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-30 21:57:54 +0800

    Create detox_eval.py

---
+++ classifiers/code/detox_eval.py
@@ -0,0 +1,105 @@
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
+import json
+from tqdm import tqdm
+from pathlib import Path
+import sys
+import argparse
+
+system_prompt = """你是一个文明表达的审查员，你的任务是判断用户输入的中文表达是否含有毒性。判断依据如下：
+
+毒性：
+1. 含有轻微脏话、不雅用词、粗俗表达。
+2. 使用变体脏话、拼音脏词、谐音粗话、委婉替代词（如"tm，特么，sb，woc，我超，鸟事"等），这些表达在语境中常用作粗俗表达。
+3. 含有明显的歧视、仇恨、侮辱、性别攻击、动物比喻式人身攻击、侮辱性动物词或恶意中伤。
+
+非毒性：
+1. 不含脏话或不雅用词。
+
+**注意**：
+- 如果句子只是包含强烈的情感极性和表达意图，如愤怒、激动、讽刺、委屈、激昂等，只要不包含毒性表达，都不能算含有毒性。
+
+输出格式：
+有毒/无毒。
+"""
+
+
+def flush(prompts, meta):
+    if not prompts:
+        return
+    outputs = llm.generate(prompts, sampling_params)
+    preds   = [o.outputs[0].text.strip() for o in outputs]
+
+    for (idx, k), p in zip(meta, preds):
+        data[idx]["tox_cls"] =p
+
+def process_one_file():
+    prompts   = []
+    meta      = []
+
+    BATCH_SZ = 128
+    for idx, item in enumerate(tqdm(data, desc="batch infer")):
+        k = "rewritten"
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user",   "content": item[k]}
+        ]
+        prompts.append(
+            tokenizer.apply_chat_template(messages, tokenize=False,
+                                        add_generation_prompt=True, enable_thinking=False)
+        )
+        meta.append((idx, k))
+
+        if len(prompts) >= BATCH_SZ:
+            flush(prompts, meta)
+            prompts, meta = [], []
+
+    flush(prompts, meta)
+
+
+    json.dump(data, open(output_path / (file.stem+"_cls.json"), "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+    print("Classification completed and saved to", output_path / (file.stem+"_cls.json"))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--folder',
+        type=str,
+        default=""
+    )
+    
+    args = parser.parse_args()
+    folder = args.folder
+
+    # Initialize the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("/home/ToxiRewriteCN/models/Qwen3-32B")
+
+    # Configurae the sampling parameters (for thinking mode)
+    sampling_params = SamplingParams (
+        temperature=0,
+        top_p=0.9,
+        top_k=20,
+        max_tokens=128
+    )
+
+    # Initialize the vLLM engine
+    llm = LLM(
+        model="/home/ToxiRewriteCN/classifiers/output/detoxification_accuracy_full_data_merged",
+        max_model_len=4096,
+        gpu_memory_utilization=0.80,
+        tensor_parallel_size=4
+    )
+
+    open_source = Path(f"./{folder}")
+    output_path = Path(f"/home/ToxiRewriteCN/classifiers/{folder}_results")
+    output_path.mkdir(parents=True, exist_ok=True)
+    for file in open_source.rglob("*.json"):
+        data = json.load(open(file, "r", encoding="utf-8"))
+        print(f"Processing {file}")
+        try:
+            process_one_file()
+        except Exception as e:
+            print(f"Error processing {file}: {e}")
+            continue

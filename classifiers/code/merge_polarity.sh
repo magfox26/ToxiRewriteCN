98958adcb3c056e7168599a4ea64f1bd5331b1bc
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-30 21:31:39 +0800

    Create merge_polarity.sh

---
+++ classifiers/code/merge_polarity.sh
@@ -0,0 +1,4 @@
+swift export \
+    --adapters /home/ToxiRewriteCN/classifiers/output/sentiment_polarity_ratio121/ \ 
+    --merge_lora true \  
+    --output_dir /home/ToxiRewriteCN/classifiers/output/merged_sentiment_polarity_ratio121/ 

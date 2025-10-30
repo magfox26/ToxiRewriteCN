60913c147d261e09c9d221041d651a41c0b04c1d
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-30 15:03:44 +0800

    Add files via upload

---
+++ finetuning_llama3-8b/code/llama3_gen.py
@@ -0,0 +1,59 @@
+import os
+import json
+from pathlib import Path
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
+from tqdm import tqdm
+
+DATA_FILE = "finetuning_llama3-8b/data/test_556.json"
+PROMPT_DIR = "finetuning_llama3-8b/prompt"
+
+USER_TEMPLATE = "输入：{sentence}"
+
+with open(DATA_FILE, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+dataset_prompts = {}
+datasets = set(item["dataset"] for item in data)
+for dataset in datasets:
+    prompt_path = os.path.join(PROMPT_DIR, f"{dataset}_prompt.txt")
+    with open(prompt_path, 'r', encoding='utf-8') as f:
+        dataset_prompts[dataset] = f.read().strip()
+
+def generate():
+    for item in tqdm(data):
+        toxic = item["toxic"]
+        dataset = item["dataset"]
+        # idx = item["idx"]
+        
+        system_prompt = dataset_prompts[dataset]
+        user_input = USER_TEMPLATE.format(sentence=toxic.strip())
+        
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_input},
+        ]
+        formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        outputs = llm.generate([formatted_prompt], sampling_params, use_tqdm=False)
+        response = outputs[0].outputs[0].text
+        item["output"] = response
+        # print(response)
+    
+    json.dump(data, open(OUTPUT_FILE, 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+
+
+
+if __name__ == "__main__":
+    model_path = "/finetuning_llama3-8b/output/llama3_8b_r1"
+
+    # Initialize the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    sampling_params = SamplingParams(temperature=1, top_p=0.5, max_tokens=32768)
+    
+
+    # Initialize the vLLM engine
+    llm = LLM(model=model_path, gpu_memory_utilization=0.8, tensor_parallel_size=1)
+    OUTPUT_FILE = "eval/llama3/test.json"
+    Path(os.path.dirname(OUTPUT_FILE)).mkdir(parents=True, exist_ok=True)
+    generate()
---
+++ finetuning_llama3-8b/code/r1_sft.sh
@@ -0,0 +1,24 @@
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+swift sft \
+--model /models/Meta-Llama-3.1-8B-Instruct \
+--model_type llama3_1 \
+--train_type full \
+--dataset /finetuning_llama3-8b/data/r1_train.json \
+--num_train_epochs 5 \
+--per_device_train_batch_size 2 \
+--per_device_eval_batch_size 2 \
+--learning_rate 1e-6 \
+--lr_scheduler_type cosine \
+--eval_strategy "epoch" \
+--gradient_accumulation_steps 2 \
+--save_total_limit 1 \
+--warmup_ratio 0.05 \
+--logging_steps 1 \
+--max_length 32768 \
+--weight_decay 1e-4 \
+--deepspeed zero3 \
+--dataloader_num_workers 4 \
+--output_dir /finetuning_llama3-8b/output/llama3_8b_r1 \
+--report_to swanlab \
+--swanlab_token your_own_swanlab_token

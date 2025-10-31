24e9475ff81750dbde78b95e1ca3138c49f8ec73
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-31 18:13:37 +0800

    Delete classifiers directory

--- classifiers/code/merge_pol.sh
+++
@@ -1,4 +0,0 @@
-swift export \
-    --adapters /home/ToxiRewriteCN/classifiers/output/pol_ratio121/ \ 
-    --merge_lora true \  
-    --output_dir /home/ToxiRewriteCN/classifiers/output/qwen3-32b-style-polarity/ 
--- classifiers/code/merge_tox.sh
+++
@@ -1,4 +0,0 @@
-swift export \
-    --adapters /home/ToxiRewriteCN/classifiers/output/detoxification_full_data/ \ 
-    --merge_lora true \  
-    --output_dir /home/ToxiRewriteCN/classifiers/output/qwen3-32b-tox-classifier/ 

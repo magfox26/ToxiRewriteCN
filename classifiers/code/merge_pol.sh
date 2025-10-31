922f26fa09d13d6fab9b24cb3e3de7d8138547d4
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-10-31 18:07:00 +0800

    Update merge_pol.sh

--- classifiers/code/merge_pol.sh
+++ classifiers/code/merge_pol.sh
@@ -1,4 +1,4 @@
 swift export \
-    --adapters /home/ToxiRewriteCN/classifiers/output/style_ratio121/ \ 
+    --adapters /home/ToxiRewriteCN/classifiers/output/pol_ratio121/ \ 
     --merge_lora true \  
     --output_dir /home/ToxiRewriteCN/classifiers/output/qwen3-32b-style-polarity/ 

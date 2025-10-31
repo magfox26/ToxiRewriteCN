96b217b553b4b1d47a646b9be5d4ac254a6637ab
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-11-01 00:43:01 +0800

    Update train_pol.sh

--- classifiers/train_pol.sh
+++ classifiers/train_pol.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+PROJECT_ROOT=$(python -c "from utils.path_utils import get_project_root; print(get_project_root())")
+
 NPROC_PER_NODE=4 \
 CUDA_VISIBLE_DEVICES=1,2,3,4 \
 swift sft \
     --model Qwen/Qwen3-32B \
     --train_type lora \
-    --dataset /home/ToxiRewriteCN/data/train_polarity_ratio121.json \
+    --dataset "$PROJECT_ROOT/data/train_polarity_ratio121.json" \ 
     --torch_dtype bfloat16 \
     --num_train_epochs 5 \
     --per_device_train_batch_size 4 \
@@ -21,9 +23,9 @@ swift sft \
     --save_total_limit 5 \
     --logging_steps 5 \
     --max_length 2048 \
-    --output_dir /home/ToxiRewriteCN/classifiers/output/pol_ratio121/ \
+    --output_dir "$PROJECT_ROOT/classifiers/output/pol_ratio121/" \  # Replace with path to save trained model
     --warmup_ratio 0.05 \
     --lora_dropout 0.05 \
     --deepspeed zero3 \
     --report_to wandb \
-    --dataloader_num_workers 4 $@ 2>&1 | tee pol_ratio121.log
+    --dataloader_num_workers 4 $@ 2>&1 | tee "$PROJECT_ROOT/classifiers/output/pol_ratio121/pol_ratio121.log"

05712dc6361c396b27e1e6a16ca9b6f748c07a14
magfox26 <166104529+magfox26@users.noreply.github.com>
2025-11-01 00:38:40 +0800

    Update merge_tox.sh

--- classifiers/merge_tox.sh
+++ classifiers/merge_tox.sh
@@ -1,4 +1,6 @@
+PROJECT_ROOT=$(python -c "from utils.path_utils import get_project_root; print(get_project_root())")
+
 swift export \
-    --adapters /home/ToxiRewriteCN/classifiers/output/detoxification_full_data/ \ 
+    --adapters "$PROJECT_ROOT/classifiers/output/detoxification_full_data/" \  # Replace with path to your LoRA adapters
     --merge_lora true \  
-    --output_dir /home/ToxiRewriteCN/classifiers/output/qwen3-32b-tox-classifier/ 
+    --output_dir "$PROJECT_ROOT/classifiers/output/qwen3-32b-tox-classifier/"  # Replace with path to save merged model
